2b0740a8974d684e8e2206ef9ed84287a37e466e
william <factzure@hotmail.com>
2021-09-09 21:11:30 -0500

    Agregar archivos de proyecto.

---
+++ projecto12_Notas/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projecto12_Notas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = 1;
+            int Nota = 0;
+            int suma = 0;
+            while (x<=8)
+            {
+                Console.WriteLine("Ingrese la nota 0" + x+" :");
+                Nota = int.Parse(Console.ReadLine());// 10 , 15 , 12
+                if (Nota >=0 && Nota<=20)
+                {
+                    suma = suma + Nota;
+                }
+                else
+                {
+                    Console.WriteLine("Nota no Valida !!");
+                    x = x - 1;
+                }
+                x = x + 1;
+            }
+            Console.WriteLine("El promedio es : {0} " + (suma/8.0));
+            Console.ReadLine();
+
+
+        }
+    }
+}
